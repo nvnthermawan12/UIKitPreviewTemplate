b2214b8c98874f26bd3981db203712d4f11ffbb1
Novianto Hermawan <nvnthermawan@gmail.com>
2022-04-15 22:29:51 +0700

    Initial Commit

---
+++ UIKitPreview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitPreview
+//
+//  Created by IDN MEDIA on 15/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
