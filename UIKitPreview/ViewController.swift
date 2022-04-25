e499139c78395ca0168caf52b2464d78d8b02f2d
Novianto Hermawan <nvnthermawan@gmail.com>
2022-04-25 11:09:25 +0700

    changing black color

--- UIKitPreview/ViewController.swift
+++ UIKitPreview/ViewController.swift
@@ -9,12 +9,12 @@ import UIKit
 import SwiftUI
 
 class ViewController: UIViewController {
-    let checkBox = CheckListView(frame: CGRect(x: 100, y: 200, width: 200, height: 200))
-    let array = ["moment","asu"]
+    let checkBox = CheckListView(frame: CGRect(x: 50, y: 200, width: 200, height: 200))
+    let array = ["moment","apakah aku"]
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        checkBox.checkBoxSetup(array, font: UIFont.systemFont(ofSize: 40), color: .blue, isExclusive: true)
+        checkBox.checkBoxSetup(array, font: UIFont.systemFont(ofSize: 40), color: .black, isExclusive: true)
         view.addSubview(checkBox)
     }
 }
